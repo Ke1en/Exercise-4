94b9e31750df2894a9b26e2cd77fc308505131a8
Ke1en <Andpey-dok@mail.ru>
2024-09-30 22:55:36 +0600

    Project updated with new cylces

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        int i = 0;
+        while (i < 5) {
+            System.out.println("i = " + i);
+            i++;
+        }
+        
+        do {
+            System.out.println("i = " + i);
+            i++;
+        } while (i < 5);
 
+        for (int j = 0; j < 5; j++) {
+            System.out.println("j = " + j);
+        }
+
+        int [] numArray = {1, 2, 3, 4, 5};
+        for (int number:numArray) {
+            System.out.println("number = " + number);
+        }
+
+        for (int z = 0; z < 5; z++) {
+            if (z == 3) {
+                System.out.println("z = " + z);
+                return;
+            }
+        }
     }
 }
